906518dfe815a79eca53791f55356be0f330dd3b
wellingtondellamura <della.mura@gmail.com>
2019-05-08 22:24:13 +0000

    Project InstaClone

---
+++ projects/InstaClone/src/instaclone/registro.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>InstaClone - Registro</title>
+    <link rel="stylesheet" href="css/style.css"/>
+  </head>
+  <body>
+    <main>
+      <section id="mainSection">
+        <header>
+          <h1>InstaClone</h1>
+          <h3>Cadastre-se para ver fotos e vídeos dos seus amigos</h3>
+        </header>
+        <div>
+          <form>
+            <div>
+              <input type="text" name="email" placeholder="Seu e-mail">
+            </div>
+            <div>
+              <input type="text" name="nome" placeholder="Nome completo">
+            </div>
+            <div>
+              <input type="text" name="usuario" placeholder="Nome de Usuário">
+            </div>
+            <div>
+              <input type="password" name="senha" placeholder="Senha">
+            </div>
+            <div>
+              <input type="checkbox" name="concordo">
+              <label for="concordo">Concordo com os termos,
+                Política de Dados e Política de Cookies.</label>
+            </div>
+            <div>
+              <input type="submit" value="Cadastre-se" >
+            </div>
+          </form>
+        </div>
+        <footer>
+           <h3>Já tem uma conta?</h3>
+           <a href="login.php">Conecte-se</a>
+        </footer>
+      </section>
+    </main>
+  </body>
+</html>
