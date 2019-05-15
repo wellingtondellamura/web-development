10ccb39dacb134d87edd88b5f7aab3a711edf060
Wellington Della Mura <della.mura@gmail.com>
2019-05-15 20:38:28 -0300

    Instaclone register form

--- projects/InstaClone/src/instaclone/registro.php
+++ projects/InstaClone/src/instaclone/registro.php
@@ -4,27 +4,34 @@
     <meta charset="utf-8">
     <title>InstaClone - Registro</title>
     <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+      integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+      crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto:400,700" rel="stylesheet">
   </head>
-  <body>
+  <body class="back-photo">
     <main>
-      <section id="mainSection">
+      <section class="panel main-panel">
         <header>
-          <h1>InstaClone</h1>
-          <h3>Cadastre-se para ver fotos e vídeos dos seus amigos</h3>
+          <h1 class="title">
+            <i class="fas fa-camera-retro"></i>
+            Insta<span class="clone">Clone</span>
+          </h1>
+          <h3 class="message">Cadastre-se para ver fotos e vídeos dos seus amigos</h3>
         </header>
-        <div>
+        <div class="form-panel">
           <form>
             <div>
-              <input type="text" name="email" placeholder="Seu e-mail">
+              <input type="text" name="email" placeholder="Número de Celular ou e-mail" class="input-control">
             </div>
             <div>
-              <input type="text" name="nome" placeholder="Nome completo">
+              <input type="text" name="nome" placeholder="Nome completo" class="input-control">
             </div>
             <div>
-              <input type="text" name="usuario" placeholder="Nome de Usuário">
+              <input type="text" name="usuario" placeholder="Nome de Usuário" class="input-control">
             </div>
             <div>
-              <input type="password" name="senha" placeholder="Senha">
+              <input type="password" name="senha" placeholder="Senha" class="input-control">
             </div>
             <div>
               <input type="checkbox" name="concordo">
@@ -32,14 +39,14 @@
                 Política de Dados e Política de Cookies.</label>
             </div>
             <div>
-              <input type="submit" value="Cadastre-se" >
+              <input type="button" value="Cadastre-se" class="button button-register" >
             </div>
           </form>
         </div>
-        <footer>
-           <h3>Já tem uma conta?</h3>
-           <a href="login.php">Conecte-se</a>
-        </footer>
+      </section>
+      <section class="panel sec-panel">
+           <h3 class="message">Já tem uma conta?</h3>
+           <a href="login.php"  class="button button-login" >Conecte-se</a>
       </section>
     </main>
   </body>
