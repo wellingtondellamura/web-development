a83b2bd466fb26a2b1135ad989cdaf92fb6d5bb9
Wellington Della Mura <della.mura@gmail.com>
2019-05-29 19:18:55 -0300

    lectures

--- projects/InstaClone/src/instaclone/registro.php
+++ projects/InstaClone/src/instaclone/registro.php
@@ -2,6 +2,7 @@
 <html lang="pt" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>InstaClone - Registro</title>
     <link rel="stylesheet" href="css/style.css"/>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
